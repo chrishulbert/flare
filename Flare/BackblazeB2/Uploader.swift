c59220d3c8aae61dccc7de9dee953335c076f3ad
Chris Hulbert <chris.hulbert@gmail.com>
2019-09-23 22:52:22 +1000

    Reuse upload urls

--- Flare/BackblazeB2/Uploader.swift
+++ Flare/BackblazeB2/Uploader.swift
@@ -11,12 +11,13 @@ import Foundation
 /// https://www.backblaze.com/b2/docs/uploading.html
 /// This is responsible for wrapping GetUploadUrl and UploadFileWrapped so that it'll retry 5 times until it finds a non-busy Backblaze pod.
 enum Uploader {
-    static func send(token: String, apiUrl: String, bucketId: String, uploadUrl: URL, fileName: String, file: Data, lastModified: Date, completion: @escaping (Result<(), Error>) -> ()) {
-        
+    /// You should pass in an uploadUrl from a previous call to GetUploadUrl, so we can reuse it until that pod is busy.
+    /// Successful completion will give you the upload url to reuse (or the url it re-fetched on a retry).
+    static func send(token: String, apiUrl: String, bucketId: String, uploadUrl: URL, fileName: String, file: Data, lastModified: Date, completion: @escaping (Result<URL, Error>) -> ()) {
         UploadFileWrapped.send(token: token, uploadUrl: uploadUrl, fileName: fileName, file: file, lastModified: lastModified, completion: { result in
             switch result {
             case .success:
-                completion(.success(()))
+                completion(.success(uploadUrl))
 
             case .needNewUploadUrl:
                 self.getUploadUrlThenUpload(failures: 1, token: token, apiUrl: apiUrl, bucketId: bucketId, fileName: fileName, file: file, lastModified: lastModified, completion: completion)
@@ -28,14 +29,14 @@ enum Uploader {
     }
     
     /// This recurses until failures is too high.
-    private static func getUploadUrlThenUpload(failures: Int, token: String, apiUrl: String, bucketId: String, fileName: String, file: Data, lastModified: Date, completion: @escaping (Result<(), Error>) -> ()) {
+    private static func getUploadUrlThenUpload(failures: Int, token: String, apiUrl: String, bucketId: String, fileName: String, file: Data, lastModified: Date, completion: @escaping (Result<URL, Error>) -> ()) {
         GetUploadUrl.send(token: token, apiUrl: apiUrl, bucketId: bucketId, completion: { result in
             switch result {
             case .success(let uploadUrl):
                 UploadFileWrapped.send(token: token, uploadUrl: uploadUrl, fileName: fileName, file: file, lastModified: lastModified, completion: { result in
                     switch result {
                     case .success:
-                        completion(.success(()))
+                        completion(.success(uploadUrl)) // Return the newly-fetched uploadUrl so it can be reused until its pod gets busy.
 
                     case .needNewUploadUrl(let error): // Backblaze pod is probably busy.
                         if failures < 5 { // Retry another time.
