94c62dcfaf95ea78f178b16850ac320d1aecaee2
Chris Hulbert <chris.hulbert@gmail.com>
2019-10-03 22:08:24 +1000

    List improvements

--- Flare/BackblazeB2/ListFileVersions.swift
+++ Flare/BackblazeB2/ListFileVersions.swift
@@ -10,9 +10,16 @@ import Foundation
 
 /// https://www.backblaze.com/b2/docs/b2_list_file_versions.html
 /// maxFileCount shouldn't be over 1000 for pricing reasons.
+/// list_file_names also exists but will not return 'hidden' files.
 /// apiUrl is eg "https://apiNNN.backblazeb2.com"
+/// delimiter is usually '/' -> https://www.backblaze.com/b2/docs/b2_list_file_names.html
+/// To get the root folder (no subfolders' files), specify delimiter=/ only.
+/// To get a subfolder, do delimiter=/ prefix=foo/bar/
+/// If you upload then hide: First record is action:hide, second is action:upload
+/// Hide record uploadTimestamp": 1568850170000 is the time of hiding.
+/// Folders are returned with names like 'photos/cats/'
 enum ListFileVersions {
-    static func send(token: String, apiUrl: String, bucketId: String, startFileName: String?, startFileId: String?, prefix: String?, completion: @escaping (Result<ListFileVersionsResponse, Error>) -> ()) {
+    static func send(token: String, apiUrl: String, bucketId: String, startFileName: String?, startFileId: String?, prefix: String?, delimiter: String?, completion: @escaping (Result<ListFileVersionsResponse, Error>) -> ()) {
         guard let url = URL(string: apiUrl + "/b2api/v2/b2_list_file_versions") else {
             completion(.failure(Service.Errors.badApiUrl))
             return
@@ -28,6 +35,9 @@ enum ListFileVersions {
         }
         if let prefix = prefix {
             body["prefix"] = prefix
+        }
+        if let delimiter = delimiter {
+            body["delimiter"] = delimiter
         }
 
         Service.shared.post(url: url, payload: body, token: token, completion: {result in
@@ -45,8 +55,8 @@ enum ListFileVersions {
 
 struct ListFileVersionsResponse {
     let files: [ListFileVersionsFile]
-    let nextFileName: String?
-    let nextFileId: String?
+    let nextFileName: String? // What to pass in to startFileName for the next search to continue where this one left off, or null if there are no more files.
+    let nextFileId: String? // What to pass in to startFileId for the next search to continue where this one left off, or null if there are no more files.
 }
 
 extension ListFileVersionsResponse {
@@ -84,4 +94,15 @@ extension ListFileVersionsFile {
                                     fileName: json["fileName"] as? String ?? "",
                                     uploadTimestamp: json["uploadTimestamp"] as? Int ?? 0)
     }
+    
+    var actionEnum: Action? {
+        return Action(rawValue: action)
+    }
+}
+
+enum Action: String {
+    case start // "start" means that a large file has been started, but not finished or canceled.
+    case upload // "upload" means a file that was uploaded to B2 Cloud Storage.
+    case hide // "hide" means a file version marking the file as hidden, so that it will not show up in b2_list_file_names.
+    case folder // "folder" is used to indicate a virtual folder when listing files.
 }
