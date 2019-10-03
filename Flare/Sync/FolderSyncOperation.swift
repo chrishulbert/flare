e5171f2f75434840d7ca5cfd7e5029ac0cc9d477
Chris Hulbert <chris.hulbert@gmail.com>
2019-10-03 22:08:46 +1000

    FolderSyncOperation wip

--- Flare/Sync/FolderSyncOperation.swift
+++ Flare/Sync/FolderSyncOperation.swift
@@ -11,14 +11,48 @@ import Foundation
 /// This syncs a single folder, and spawns ops for its subfolders.
 class FolderSyncOperation: AsyncOperation {
     let syncContext: SyncContext
-    let path: String
-    init(syncContext: SyncContext, path: String) {
+    let path: String? // Nil for root folder, otherwise something like 'foo/bar/'
+    init(syncContext: SyncContext, path: String?) {
         self.syncContext = syncContext
         self.path = path
         super.init()
     }
     
     override func asyncStart() {
-        <#code#>
+        guard let auth = syncContext.authorizeAccountResponse else {
+            print("Missing auth token")
+            exit(EXIT_FAILURE)
+        }
+        
+        ListAllFileVersions.send(token: auth.authorizationToken, apiUrl: auth.apiUrl, bucketId: syncContext.config.bucketId, prefix: path, delimiter: "/", completion: { [weak self] result in
+            guard let sself = self else { return }
+            
+            switch result {
+            case .success(let files):
+                for file in files {
+                    guard let action = file.actionEnum else { continue }
+                    switch action {
+                    case .start:
+                        // In progress, so don't touch anything - this file can be taken care of next time the sync runs.
+                        print("Ignoring \(file.fileName) because it's an in-progress upload")
+                        
+                    case .upload:
+                        <#code#>
+                        
+                    case .hide:
+                        <#code#>
+                        
+                    case .folder: // Queue subfolders.
+                        let op = FolderSyncOperation(syncContext: sself.syncContext, path: file.fileName)
+                        SyncManager.shared.finalOperation.addDependency(op) // Ensure my new op runs before the 'final' one.
+                        SyncManager.shared.queue.addOperation(op)
+                    }
+                }
+                
+            case .failure(let error):
+                print("FolderSyncOperation > ListAllFileVersions error: \(error)")
+                exit(EXIT_FAILURE)
+            }
+        })
     }
 }
