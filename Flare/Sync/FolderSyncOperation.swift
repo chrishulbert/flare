e0ff61c3c298c8c3a57e57194aa22eae5baa7c87
Chris Hulbert <chris.hulbert@gmail.com>
2020-03-06 22:11:47 +1100

    Refactor

--- Flare/Sync/FolderSyncOperation.swift
+++ Flare/Sync/FolderSyncOperation.swift
@@ -10,55 +10,34 @@ import Foundation
 
 /// This syncs a single folder, and spawns ops for its subfolders.
 enum FolderSyncOperation {
-    enum Errors: Error {
-        case nilAuthToken
-        case nilIsDirectory
-        case nilContentModificationDate
-        case nilFileSize
-    }
     
     /// Path is nil for root folder, otherwise something like 'foo/bar/' with a trailing slash.
     /// Returns subfolders.
     static func sync(path: String?, syncContext: SyncContext) throws -> [String] {
-        guard let auth = syncContext.authorizeAccountResponse else {
-            throw Errors.nilAuthToken
-        }
-        
         let remoteState = try RemoteSyncListing.list(path: path, syncContext: syncContext)
+        let localState = try LocalSyncListing.list(path: path, syncContext: syncContext)
         
         // Next collect a list of 'local' state.
-        // Contents of directory doesn't return '._*' files eg ds store
+        
         // TODO make it skip files that can't be accessed locally eg they're being saved or something, eg mark them as 'skip this!', deal with them next time around?
-        let contents = try FileManager.default.myContents(ofDirectory: syncContext.pathUrl(path: path))
-        var localStates: [String: SyncFileState] = [:]
-        let rootUrl = URL(fileURLWithPath: syncContext.config.folder)
-        for file in contents {
-            let filePathRelativeToRoot = file.absoluteString.deleting(prefix: rootUrl.absoluteString) // For folders, this'll give us a trailing slash which is what we need to suit bz.
-            print("Getting resource values for \(file)")
-            let resourceValues = try file.resourceValues(forKeys: [.isDirectoryKey, .contentModificationDateKey, .fileSizeKey])
-            guard let isDirectory = resourceValues.isDirectory else { throw Errors.nilIsDirectory }
-            guard let contentModificationDate = resourceValues.contentModificationDate else { throw Errors.nilContentModificationDate }
+        // TODO we *need* the watcher to get an accurate idea of local deletions.
+        // TODO Maybe upon local deletion, we 'hide' remotely but don't even do a full sync or anything.
+        // TODO for local deletions, think about how to model folder deletes, given that if we simply make `.deleted.DATE.FILENAME` in the same folder it'll be lost. Instead create 'FlareRoot/relevant/folder/here/.deleted.foo.filename ? And for sync deletions, *move* to same file?
+        // TODO Or maybe on local deletion, create a .deleted.XFILENAME file which this'll then pick up and remove once synced.
+        // TODO When sync deletes a file, move it to a 'deleted' folder eg yyyymmd_filename which gets nuked once >1mo.
 
-            if isDirectory { // Add to a set of subfolders, along with local subfolders, so we don't add 2 operations for one folder.
-                subfolders.insert(filePathRelativeToRoot) // TODO include the contentModificationDate for comparison.
-            } else {
-                guard let fileSize = resourceValues.fileSize else { throw Errors.nilFileSize } // Can only get filesize if not a dir.
-                localStates[filePathRelativeToRoot] = .exists(contentModificationDate, fileSize, nil)
-            }
-            // TODO we *need* the watcher to get an accurate idea of local deletions.
-            // TODO Maybe upon local deletion, we 'hide' remotely but don't even do a full sync or anything.
-            // TODO for local deletions, think about how to model folder deletes, given that if we simply make `.deleted.DATE.FILENAME` in the same folder it'll be lost. Instead create 'FlareRoot/relevant/folder/here/.deleted.foo.filename ? And for sync deletions, *move* to same file?
-            // TODO Or maybe on local deletion, create a .deleted.XFILENAME file which this'll then pick up and remove once synced.
-            // TODO When sync deletes a file, move it to a 'deleted' folder eg yyyymmd_filename which gets nuked once >1mo.
-        }
-
+        // Create a list of every potential file.
+        var remotePlusLocalFiles: Set<String> = Set(localState.files.keys).union(remoteState.files.keys)
+        remotePlusLocalFiles.subtract(remoteState.filesToSkip)
+        remotePlusLocalFiles.subtract(localState.filesToSkip)
+        
         // Finally reconcile.
         var actions: [SyncAction] = []
-        let remotePlusLocalFiles: Set<String> = Set(localStates.keys).union(remoteStates.keys)
+        // TODO remove all 'skip' files from above set.
         let oneMonthAgo = Date().addingTimeInterval(-30*24*60*60)
         for file in remotePlusLocalFiles {
-            let localState = localStates[file] ?? .missing
-            let remoteState = remoteStates[file] ?? .missing
+            let localState = localState.files[file] ?? .missing
+            let remoteState = remoteState.files[file] ?? .missing
             switch (localState, remoteState) {
             case (.exists(let localDate, let localSize, let localSha1), .exists(let remoteDate, let remoteSize, let remoteSha1)):
                 // Do the 3 date comparisons: same/earlier/later:
